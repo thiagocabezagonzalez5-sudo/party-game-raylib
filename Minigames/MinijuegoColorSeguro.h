f165b1b399960aa5b51cdf386d3b06b5ca50e56f
thiagocabezagonzalez5-sudo <thiagocabezagonzalez5@gmail.com>
2026-09-03 15:27:00 -0300

    Color Seguro: usar combate sin empujes

--- Minigames/MinijuegoColorSeguro.h
+++ Minigames/MinijuegoColorSeguro.h
@@ -3,6 +3,7 @@
 #include "Core/Participante.h"
 #include "Core/ResultadoMinijuego.h"
 #include "Minigames/TiposMinijuegos.h"
+#include "Minigames/CombateJugadores.h"
 
 
 const int CANTIDAD_PLATAFORMAS_COLOR = 7;
@@ -100,3 +101,12 @@ struct MinijuegoColorSeguro
 
     const ResultadoMinijuego& ObtenerResultado() const;
 };
+
+
+// En los minijuegos de jugadores normales usamos colision
+// solida sin empuje y golpes con ralentizacion/particulas.
+// MinijuegoPelotas.cpp no incluye este encabezado, por lo que
+// conserva ResolverColisionesPelotas() y su fisica de empuje.
+#define ActualizarJugadorPrueba ActualizarJugadorPruebaConEfectos
+#define ResolverColisionesJugadoresNormales ResolverColisionesJugadoresSinEmpuje
+#define ResolverGolpesJugadores ResolverGolpesJugadoresConEfectos
